344aa53d4771bb3e3beb93f04047dcac2df8faa5
albemala <albemala@gmail.com>
2024-09-10 15:37:36 +0200

    added fonts to assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:colourlovers_app/app/view.dart';
 import 'package:colourlovers_app/preferences/data-controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // GoogleFonts.config.allowRuntimeFetching = false;
+  GoogleFonts.config.allowRuntimeFetching = false;
 
   runApp(
     MultiBlocProvider(
