244a5fe117aea0e736ccb28812e9855d1c7e41de
albemala <albemala@gmail.com>
2025-06-04 14:01:01 +0200

    restored previous package name and app name

--- android/app/src/main/kotlin/me/albemala/colourlovers_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package me.albemala.colourlovers_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/me/albemala/luv/MainActivity.kt
@@ -0,0 +1,5 @@
+package me.albemala.luv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
