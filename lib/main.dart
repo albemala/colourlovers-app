69ab04b3d02258795a5a332b888913d02ebf638f
albemala <albemala@gmail.com>
2025-06-05 15:40:35 +0200

    forgot to add sentry to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:colourlovers_app/analytics.dart';
 import 'package:colourlovers_app/app/view.dart';
 import 'package:colourlovers_app/favorites/data-controller.dart';
 import 'package:colourlovers_app/filters/color/data-controller.dart';
@@ -9,45 +10,51 @@ import 'package:colourlovers_app/preferences/data-controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   GoogleFonts.config.allowRuntimeFetching = false;
 
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => FavoritesDataController(),
-          lazy: false, //
+  await SentryFlutter.init(
+    configureSentry,
+    appRunner: () {
+      runApp(
+        MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (_) => FavoritesDataController(),
+              lazy: false, //
+            ),
+            BlocProvider(
+              create: (_) => PreferencesDataController(),
+              lazy: false, //
+            ),
+            BlocProvider(
+              create: (_) => ColorFiltersDataController(),
+              lazy: false, //
+            ),
+            BlocProvider(
+              create: (_) => PaletteFiltersDataController(),
+              lazy: false, //
+            ),
+            BlocProvider(
+              create: (_) => PatternFiltersDataController(),
+              lazy: false, //
+            ),
+            BlocProvider(
+              create: (_) => UserFiltersDataController(),
+              lazy: false, //
+            ),
+            BlocProvider(
+              create: (_) => FavoritesFiltersDataController(),
+              lazy: false, //
+            ),
+          ],
+          child: const AppViewCreator(),
         ),
-        BlocProvider(
-          create: (_) => PreferencesDataController(),
-          lazy: false, //
-        ),
-        BlocProvider(
-          create: (_) => ColorFiltersDataController(),
-          lazy: false, //
-        ),
-        BlocProvider(
-          create: (_) => PaletteFiltersDataController(),
-          lazy: false, //
-        ),
-        BlocProvider(
-          create: (_) => PatternFiltersDataController(),
-          lazy: false, //
-        ),
-        BlocProvider(
-          create: (_) => UserFiltersDataController(),
-          lazy: false, //
-        ),
-        BlocProvider(
-          create: (_) => FavoritesFiltersDataController(),
-          lazy: false, //
-        ),
-      ],
-      child: const AppViewCreator(),
-    ),
+      );
+    },
   );
 }
