d576f71620331a56de517118eff99e7e351c91a9
albemala <albemala@gmail.com>
2025-12-11 16:08:13 +0100

    updated urls

--- lib/urls.dart
+++ lib/urls.dart
@@ -4,13 +4,21 @@ import 'package:url_launcher/url_launcher.dart';
 
 const supportEmailUrl = 'albemala@gmail.com';
 
-const websiteUrl = 'https://luv.albemala.me/';
+// const websiteUrl = 'https://luv.albemala.me/';
 const repositoryUrl = 'https://github.com/albemala/colourlovers-app';
 const twitterUrl = 'https://twitter.com/albemala';
 
 const colourLoversUrl = 'https://www.colourlovers.com';
 const colourLoversLicenseUrl = '$colourLoversUrl/api';
 const creativeCommonsUrl = 'https://creativecommons.org/licenses/by-nc-sa/3.0/';
+
+String get websiteUrl => switch (Platform.operatingSystem) {
+  'ios' ||
+  'macos' => 'https://apps.apple.com/us/app/color-picker-luv/id1438312561',
+  'android' =>
+    'https://play.google.com/store/apps/details?id=me.albemala.luv&hl=en',
+  _ => 'https://github.com/albemala/colourlovers-app',
+};
 
 String get otherProjectsUrl {
   switch (Platform.operatingSystem) {
@@ -19,34 +27,6 @@ String get otherProjectsUrl {
       return 'https://apps.apple.com/us/developer/alberto-malagoli/id965971566';
     default:
       return 'https://projects.albemala.me/?ref=luv-app';
-  }
-}
-
-const hexeeWebsiteUrl = 'https://hexee.app/?ref=luv-app';
-const wmapWebsiteUrl = 'https://wmap.albemala.me/?ref=luv-app';
-const iroIroWebsiteUrl = 'https://iro-iro.albemala.me/?ref=luv-app';
-
-String get ejimoWebsiteUrl {
-  switch (Platform.operatingSystem) {
-    case 'ios':
-    case 'macos':
-      return 'https://apps.apple.com/us/app/ejimo/id1598944603';
-    case 'android':
-      return 'https://play.google.com/store/apps/details?id=me.albemala.ejimo';
-    default:
-      return 'https://ejimo-app.web.app'; // Default for web and other platforms
-  }
-}
-
-String get upcWebsiteUrl {
-  switch (Platform.operatingSystem) {
-    case 'ios':
-    case 'macos':
-      return 'https://apps.apple.com/us/app/universal-palette-converter/id6480113031';
-    case 'android':
-      return 'https://play.google.com/store/apps/details?id=me.albemala.paletteconverter';
-    default:
-      return '';
   }
 }
 
